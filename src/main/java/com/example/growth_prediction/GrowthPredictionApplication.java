efe51100463e8afee75f6c3b2935d1381eb7b5ac
Choi Hyoung Jun <chsam2@ajou.ac.kr>
2022-05-09 15:02:20 +0900

    setting

---
+++ src/main/java/com/example/growth_prediction/GrowthPredictionApplication.java
@@ -0,0 +1,13 @@
+package com.example.growth_prediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowthPredictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowthPredictionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/growth_prediction/GrowthPredictionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.growth_prediction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowthPredictionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
